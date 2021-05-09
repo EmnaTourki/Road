eadfe79acd37a3727b750d3ed3629fd433f5bf97
ayazghari <aya.zghari@epfl.ch>
2021-05-09 16:20:12 +0200

    add passage pieton camera 2.0

---
+++ process_image.h
@@ -0,0 +1,26 @@
+#ifndef PROCESS_IMAGE_H
+#define PROCESS_IMAGE_H
+
+//constants for the differents parts of the project
+#define IMAGE_BUFFER_SIZE		640
+#define WIDTH_SLOPE				5
+#define MIN_LINE_WIDTH			40
+#define ROTATION_THRESHOLD		10
+#define ROTATION_COEFF			2
+#define PXTOCM					1570.0f //experimental value
+#define GOAL_DISTANCE 			10.0f
+#define MAX_DISTANCE 			25.0f
+#define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
+#define KP						800.0f
+#define KI 						3.5f	//must not be zero
+#define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
+
+
+
+
+
+uint16_t get_lineWidth(void);
+bool passage_pieton_detected(void);
+void process_image_start(void);
+
+#endif /* PROCESS_IMAGE_H */
